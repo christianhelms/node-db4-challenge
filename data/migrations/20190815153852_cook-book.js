1e14ad1bb0e766d7beb66188f1ba8a77df1e977e
christianhelms <helmsmedia@gmail.com>
2019-08-15 15:53:45 -0500

    cook-book migration and cleanup seed added

--- data/migrations/20190815153852_cook-book.js
+++ data/migrations/20190815153852_cook-book.js
@@ -1,8 +1,46 @@
-
 exports.up = function(knex) {
+    return knex.schema
+      .createTable('recipes', tbl => {
+        tbl.increments();
   
-};
-
-exports.down = function(knex) {
+        tbl
+          .string('recipe_name', 255)
+          .notNullable()
+          .unique();
+      })
+      .createTable('ingredients', tbl => {
+        tbl.increments();
   
-};
+        tbl
+          .string('ingredient_name', 255)
+          .notNullable()
+      })
+      .createTable('recipe_ingredients', tbl => {
+        tbl.increments();
+  
+        tbl
+          .integer('recipe_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('recipes')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+        tbl
+          .integer('ingredient_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('ingredients')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE'); // if the PK on primary table changes, what to do?
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('recipe_ingredients')
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('recipes');
+  };
+  
